bfec005deef0e0b290e984c97a30a3ddd4aca4cd
sarahwonago <sarahshaaleh@gmail.com>
2025-08-11 12:41:46 +0300

    Initial Setup and Project Configuration for a Django Blog

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
@@ -6,7 +7,7 @@ class Post(models.Model):
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
-        "auth.User",
+        User,
         on_delete=models.CASCADE,
     )
 
